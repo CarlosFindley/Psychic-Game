2804890dca6b743eb1d77f427f1f98b92406f720
Carlos Villalta <CarlosFindley@protonmail.com>
2018-10-07 00:19:08 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,11 @@
 //Letter choices available
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Setting all to zero
+//Creating variables and setting them to zero
 let wins = 0;
 let losses = 0;
-let guesses = 7;
 let guessesLeft = 7;
 let guessedLetters = [];
-var letterToGuess = null;
 
 //Lets the computer select a random letter from the available choices
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
