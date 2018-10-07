bf4ee97e289a87f3019a6ceaf1ef9f4d72b5211b
Carlos Villalta <CarlosFindley@protonmail.com>
2018-10-07 13:02:43 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
-// Letter choices available
+// I based my work on help I found online, specifically https://github.com/Ednas/Psychic-Game
+
+// Array of letters available
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Creating variables and setting them to zero
@@ -7,66 +9,63 @@ let losses = 0;
 let guessesLeft = 7;
 let guessedLetters = [];
 
-// Lets the computer select a random letter from the available choices
+// The computer selects a random letter from the available choices
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-// Captures how many guesses are left
-function updateGuessesLeft() {
-    // Dig into DOM node tree and grab guessesLeft to output guesses left
+// This one updates the "guesses left" in HTML
+function updateGuessCount() {
     document.querySelector(".guessLeft").innerHTML = "Guesses left: " + guessesLeft;
-};
+}
 
-// 
 function updateLetterToGuess() {
     this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-};
+}
 
+// This one updates the "your guesses so far" in HTML
 function updateGuessesSoFar() {
-    // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
     document.querySelector(".guessSoFar").innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-};
+}
 // Function will be called when we reset everything
 var reset = function() {
-    totalGuesses = 7;
-    guessesLeft = 7;
+    guessesLeft = 9;
     guessedLetters = [];
 
     updateLetterToGuess();
-    updateGuessesLeft();
+    updateGuessCount();
     updateGuessesSoFar();
 }
 
 updateLetterToGuess();
-updateGuessesLeft();
+updateGuessCount();
 
 //When key is released it becomes the users guess
 document.onkeyup = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     var check = computerChoices.includes(userGuess);
 
+    // If wrong guess
     if (check === false) {
         alert("Not quite, try again.");
         return false;
     } else if (check === true) {
-        //If the Users choice was an alphabet char then update guesses left and add users guess to the array of guessed letters
         guessesLeft--;
         guessedLetters.push(userGuess);
-        updateGuessesLeft();
+        updateGuessCount();
         updateGuessesSoFar();
 
+        // If right guess
         if (guessesLeft > 0) {
             if (userGuess == letterToGuess) {
                 wins++;
-                document.querySelector("wins").innerHTML = "Wins: " + wins;
+                document.querySelector(".wins").innerHTML = "Wins: " + wins;
                 userGuess = userGuess.toUpperCase();
-                alert("You are LUCKY! Are you a psychic?" + userGuess);
+                alert("You are LUCKY! The right letter is " + "\"" + computerGuess + "\"");
                 reset();
             }
-        } else if (guessesLeft == 0) {
-            // Then we will loss and we'll update the html to display the loss 
+        } else if (guessesLeft == 0) { // captures the losses
             losses++;
             document.querySelector(".losses").innerHTML = "Losses: " + losses;
-            alert("How UNFORTUNATE!");
+            alert("How UNFORTUNATE! The right letter is " + "\"" + computerGuess + "\"");
             // Then we'll call the reset. 
             reset();
         }
@@ -75,4 +74,4 @@ document.onkeyup = function(event) {
         alert("Oops, we have an error");
     }
 
-};
+}
