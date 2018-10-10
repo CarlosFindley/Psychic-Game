7b8a77af318b6eb350346902553b7a431a33330b
Carlos Villalta <CarlosFindley@protonmail.com>
2018-10-10 01:44:49 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,40 @@
-// I based my work on help I found online, specifically https://github.com/Ednas/Psychic-Game
+// Create array of choices for the computer to choose from
+var arrayOfLetters = ["a", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Array of letters available
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// Setting score defaults
+var wins = 0;
+var losses = 0;
+var guessesLeft = 7;
+var guessSoFar = [];
 
-// Creating variables and setting them to zero
-let wins = 0;
-let losses = 0;
-let guessesLeft = 7;
-let guessedLetters = [];
+// Create variables that hold references to the places in the HTML where we want to display things.
+document.querySelector(".wins").innerHTML = "Wins: " + wins;
+document.querySelector(".losses").innerHTML = "Losses: " + losses;
+document.querySelector(".guessLeft").innerHTML = "Guesses Left: " + guessesLeft;
+document.querySelector(".guessSoFar").innerHTML = "Your Guesses so far: " + guessSoFar;
 
-// The computer selects a random letter from the available choices
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-// This one updates the "guesses left" in HTML
-function updateGuessCount() {
-    document.querySelector(".guessLeft").innerHTML = "Guesses left: " + guessesLeft;
-}
-
-function updateLetterToGuess() {
-    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-}
-
-// This one updates the "your guesses so far" in HTML
-function updateGuessesSoFar() {
-    document.querySelector(".guessSoFar").innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-}
-// Function will be called when we reset everything
-var reset = function() {
-    guessesLeft = 9;
-    guessedLetters = [];
-
-    updateLetterToGuess();
-    updateGuessCount();
-    updateGuessesSoFar();
-}
-
-updateLetterToGuess();
-updateGuessCount();
-
-//When key is released it becomes the users guess
+// This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    var check = computerChoices.includes(userGuess);
 
-    // If wrong guess
-    if (check === false) {
-        alert("Not quite, try again.");
-        return false;
-    } else if (check === true) {
-        guessesLeft--;
-        guessedLetters.push(userGuess);
-        updateGuessCount();
-        updateGuessesSoFar();
+    // Determines which key was pressed.
+    var userGuess = event.key;
 
-        // If right guess
-        if (guessesLeft > 0) {
-            if (userGuess == letterToGuess) {
-                wins++;
-                document.querySelector(".wins").innerHTML = "Wins: " + wins;
-                userGuess = userGuess.toUpperCase();
-                alert("You are LUCKY! The right letter is " + "\"" + computerGuess + "\"");
-                reset();
-            }
-        } else if (guessesLeft == 0) { // captures the losses
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = arrayOfLetters[Math.floor(Math.random() * arrayOfLetters.length)];
+
+        // I user guess and computer guess match
+        if (userGuess === computerGuess) {
+            wins++;
+            // wins.textContent = "Wins: " + wins;
+        } else if (userGuess !== computerGuess) {
             losses++;
-            document.querySelector(".losses").innerHTML = "Losses: " + losses;
-            alert("How UNFORTUNATE! The right letter is " + "\"" + computerGuess + "\"");
-            // Then we'll call the reset. 
-            reset();
+            
         }
-        return false;
-    } else {
-        alert("Oops, we have an error");
-    }
 
-}
+        document.querySelector(".wins").innerHTML = "Wins: " + wins;
+        document.querySelector(".losses").innerHTML = "Losses: " + losses;
+        document.querySelector(".guessLeft").innerHTML = "Guesses Left: " + guessesLeft;
+        document.querySelector(".guessSoFar").innerHTML = "Your Guesses so far: " + guessSoFar;
+
+
+  };
